71914b997b167df53b7305238f4dcc91c8e601b4
https://github.com/thanhnhaduong/A1121i1--D-ng-Thanh-Nh-.git <nhacc8@gmail.com>
2022-02-18 21:01:12 +0700

    up đầu năm

--- module 2/demo/src/ss1_jvoverview/thuchanh/Sudung_Toantu.java
+++ module 2/demo/src/ss1_jvoverview/thuchanh/Sudung_Toantu.java
@@ -6,16 +6,7 @@ public class Sudung_Toantu {
     public static void main(String[] args) {
         float width;
         float height;
+    Scanner dientich = new Scanner(System.in)
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter width: ");
-        width = scanner.nextFloat();
-
-        System.out.println("Enter height: ");
-        height = scanner.nextFloat();
-        float area = width * height;
-
-        System.out.println("Area is: " + area);
     }
 }
