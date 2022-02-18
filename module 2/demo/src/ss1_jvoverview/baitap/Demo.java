f6982b98b94a8bc5201a898fbd782f0b6b14f89d
https://github.com/thanhnhaduong/A1121i1--D-ng-Thanh-Nh-.git <nhacc8@gmail.com>
2022-02-18 20:56:42 +0700

    up đầu năm

---
+++ module 2/demo/src/ss1_jvoverview/baitap/Demo.java
@@ -0,0 +1,6 @@
+package ss1_jvoverview.baitap;
+
+public class Demo {
+    public static void main(String[] args)
+    { System.out.println("hello");}
+}
---
+++ module 2/demo/src/ss1_jvoverview/thuchanh/Time_date.java
@@ -0,0 +1,10 @@
+package ss1_jvoverview.thuchanh;
+
+import java.util.Date;
+
+public class Time_date {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("now is " + now);
+    }
+}
